654952f8801501a54f68517fc47723cc46b681bc
andylithia <thelithcore@gmail.com>
2022-11-20 00:13:22 -0500

    DAC controller started

--- Intel/Quartus/DACcon.v
+++ Intel/Quartus/DACcon.v
@@ -22,13 +22,87 @@
 //==============================================================================
 // Synchronous DAC Controller
 // 12-bit Internal Resolution, 8-bit output resolution
+//
+// Operating Modes:
+// Unified Square / Pulse / Sawtooth / Triangle Wave Generator
+//            .-----.               .-----.               .-----.        
+//           /       \             /       \             /       \       
+//          / |    |  \           /         \           /         \       
+//         /           \         /           \         /           \       
+//        /   |    |    \       /             \       /             \       
+//  -----'               '-----'               '-----'               '-----
+//       | A |  B  | C  |  D  |
+// 
+// Value of A, B, C and D should be pre-computed and stored in an LUT
+// The amplitude is not explicitly specified. Instead it's controlled by duration
+// of the A and C ramps. 
+//
+// Fast Clock: 384MHz
+// Desired chirp interval: 2ms
+// Number of cycles usable: 768000 -> 20bits
+//
 module DACcon(
-    input         clk_fast,
+    input         clk_fast, // Note: The system runs on 384MHz
+                            // The reported fmax is smaller than this (282MHz), 
+                            // but at room temperature this should be fine.
     input         rst_n,
     input [1:0]   mode,
+    input [19:0]  A_hlen,
+    input [19:0]  A_hstep,
+    input [11:0]  A_vincr,
+    input [19:0]  B_hlen,
+    input [19:0]  B_hstep,
+    input [11:0]  B_vincr,
+    input [19:0]  C_hlen,
+    input [19:0]  C_hstep,
+    input [11:0]  C_vincr,
+    input [19:0]  D_hlen,
+    input [19:0]  D_hstep,
+    input [11:0]  D_vincr,
     output [7:0]  dac_out,
-    output [11:0] raw_out,
-
+    output [11:0] raw_out
 );
+
+    reg [1:0]  ss_r;
+    reg [19:0] hcnt_r;
+    reg [11:0] vcnt_r;
+
+    assign raw_out = vcnt_r;
+
+    localparam S_A = 2'b00;
+    localparam S_B = 2'b01;
+    localparam S_C = 2'b11;
+    localparam S_D = 2'b10;
+    always @(posedge clk_fast or negedge rst_n) begin
+        if(~rst_n) begin
+            ss_r <= S_A;
+            hcnt_r <= 0;       
+            vcnt_r <= 0;       
+        end else begin
+            if (ss_r == S_A) begin
+                if(A_hstep + {1'b0, hcnt_r} > {1'b0,A_hlen}) begin
+                    // Overflowing, jump to the next state
+                    vcnt_r <= 0;
+                    hcnt_r <= 0;
+                    if(B_hlen!=0)        ss_r <= S_B;
+                    else if (C_hlen!=0)  ss_r <= S_C;
+                    else if (D_hlen!=0)  ss_r <= S_D;
+                    else                 ss_r <= S_A;
+                end else begin
+                    hcnt_r <= hcnt_r + A_hstep;
+
+                end
+            end else if (ss_r == S_B) begin
+                
+            end else if (ss_r == S_C) begin
+
+            end else if (ss_r == S_D) begin
+
+            end else begin
+                // Should never happen
+                ss_r <= 2'bxx;
+            end
+        end
+    end
 
 endmodule /* DACcon */
---
+++ Intel/Quartus/DACcon_saw.v
@@ -0,0 +1,90 @@
+// MIT License
+// 
+// Copyright (c) 2022 andylithia
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//==============================================================================
+// Synchronous DAC Controller - Sawtooth
+// 12-bit Internal Resolution, 8-bit output resolution
+//
+// Operating Modes:
+// Unified Square / Pulse / Sawtooth / Triangle Wave Generator
+//            /|     /|     /|     /| 
+//           / |    / |    / |    / | 
+//          /  |   /  |   /  |   /  | 
+//         /   |  /   |  /   |  /   | 
+//        /    | /    | /    | /    | 
+//  -----'     |/     |/     |/     |/
+// 
+//
+// Usage Example:
+// Expected output: Full Range, 2ms chirp
+// 1.To maximize vertical resolution, set aincr = 1
+// 2.Steps needed: (2**12)/aincr = 4096
+// 3.Division ratio when the fast clock is 384MHz: (384MHz*2ms)/N_steps= 187.5
+// Oops, fractal-N division ratio is not supported yet. It requires another SDM
+// Falling back to step 1:
+// Set aincr = 2
+// Steps needed: tlen   = 2048
+// Division ratio: tstep = 375
+// 
+// Fast Clock: 384MHz
+// Desired chirp interval: 2ms
+// Number of cycles usable: 768000 -> 20bits
+//
+module DACcon_saw(
+    input         clk_fast,
+    input         rst_n,
+    input [19:0]  A_len,    // Total number of steps
+    input [19:0]  A_tstep,  // Clock-cycle per step
+    input [11:0]  A_aincr,  // Vertical increment per step
+    output [11:0] out_raw,  //
+    output [7:0]  out_sdm,  // Sigma-Delta Dithered Output (8bits)
+    output        overflow
+);
+
+    reg [1:0]  ss_r;
+    reg [19:0] clkdiv_r;
+    reg [19:0] treg_r;
+    reg [12:0] areg_r;
+    assign overflow = areg_r[12];
+    assign out_raw  = areg_r[11:0];
+
+    always @(posedge clk_fast or negedge rst_n) begin
+        if(~rst_n) begin
+            clkdiv_r <= 0;
+            treg_r   <= 0;
+            areg_r   <= 0;
+        end else begin
+            if (clkdiv_r < A_tstep) begin
+                clkdiv_r <= clkdiv_r + 1;               
+            end else begin
+                clkdiv_r <= 0;
+                if (treg_r >= A_len) begin
+                    treg_r <= 0;
+                    areg_r <= 0;
+                end else begin  
+                    treg_r <= treg_r + 1;
+                    areg_r <= areg_r + A_aincr; 
+                end
+            end
+        end
+    end
+
+endmodule /* DACcon_saw */
